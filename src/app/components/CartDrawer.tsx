ebf47539f00335e84ef8fd4f1ff10d1ecb11110d
Safal Shrestha <sshrestha24@wooster.edu>
2025-06-07 13:11:47 -0400

    more changes

---
+++ src/app/components/CartDrawer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CartDrawer() {
+  return (
+    <div>CartDrawer</div>
+  )
+}
+
+export default CartDrawer
---
+++ src/app/components/ShoeCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShoeCard() {
+  return (
+    <div>ShoeCard</div>
+  )
+}
+
+export default ShoeCard
